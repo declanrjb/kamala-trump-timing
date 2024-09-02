fdfc79f9177fa25c06bbed6dbeafa022789640c1
declanrjb <declanrjb@gmail.com>
2024-09-02 16:47:45 -0400

    change to one tweet at a time model

--- web-app/script.js
+++ web-app/script.js
@@ -14,16 +14,22 @@ function createTweet(container,tweetContent,top=0) {
     return(newTweet)
 }
 
+function updateTweet(tweetObj,tweetContent,top=0) {
+    tweetObj.children('.tweet-content').html(tweetContent)
+    tweetObj.css('top',top)
+}
+
 function tagLine(line,tweet) {
     $('.speech-para').each(function() {
         if ($(this).html().replace(/\s+/g,' ').trim().includes(line)) {
             var selectedText = highlightText($(this),line)
-
+/*
             if ((annotCounter % 2) == 0) {
                 var newTweet = createTweet($('#posts-container-left'),tweet,selectedText.position().top)
             } else {
                 var newTweet = createTweet($('#posts-container-right'),tweet,selectedText.position().top)
             }
+*/
 
             annotCounter += 1
             /*
@@ -51,6 +57,25 @@ function tagLine(line,tweet) {
     })
 }
 
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+function highestVisible(selector) {
+    var elems = $(selector)
+    var found = null;
+    elems.each(function() {
+        if ($(this).isInViewport()) {
+            if (found == null) {
+                found = this
+            }
+        }
+    })
+    return(found)
+}
+
 var annotCounter = 0;
 
 $(function() {
@@ -61,10 +86,12 @@ $(function() {
     }
 
     $.getJSON('quote-matches.json',function(data) {
+        var annotsDict = {}
         $(data).each(function(row) {
             var harris_line = data[row]['harris line match']
             if (harris_line != null) {
                 harris_line = harris_line.trim()
+                annotsDict[harris_line] = data[row]['text_content']
                 tagLine(harris_line,data[row]['text_content'])
             }
         })
@@ -74,22 +101,35 @@ $(function() {
             var matchedTweet = $('#tweet-' + $(this).attr('id').split('-')[1])
 
             $(this).on('mouseenter',function() {
-                $(this).css('background-color','orange')
+                $(this).css('background-color','lightgrey')
                 $(matchedTweet).css('box-shadow','0 5px 5px grey')
+                $(matchedTweet).css('z-index',99)
             })
             $(this).on('mouseleave',function() {
-                $(this).css('background-color','yellow')
+                $(this).css('background-color','transparent')
                 $(matchedTweet).css('box-shadow','0 1px 5px lightgrey')
+                $(matchedTweet).css('z-index',99)
             })
 
             $(matchedTweet).on('mouseenter',function() {
-                $(currAnnot).css('background-color','orange')
+                $(currAnnot).css('background-color','lightgrey')
                 $(this).css('box-shadow','0 5px 5px grey')
+                $(this).css('z-index',99)
             })
             $(matchedTweet).on('mouseleave',function() {
-                $(currAnnot).css('background-color','yellow')
+                $(currAnnot).css('background-color','transparent')
                 $(this).css('box-shadow','0 1px 5px lightgrey')
+                $(this).css('z-index',0)
             })
         })
+
+        var mainTweet = $('#template-tweet')
+
+        $(window).scroll(function() {
+            var highestAnchor = $(highestVisible('.annotation'))
+            updateTweet(mainTweet,annotsDict[highestAnchor.text()],highestAnchor.position().top)
+        })
     })
+
+    
 })
